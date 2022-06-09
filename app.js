7e65857c7c8ace43849cf071b36cdf7a4bf7c07e
cristianbl <a25340@svalero.com>
2022-06-09 03:28:09 +0200

    modify port env

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { json } = require('express');
 const express = require('express');
 const app = express();
+var port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +10,6 @@ app.use(express(json));
 
 app.use('/', require('./router'));
 
-app.listen(5000, ()=>{
+app.listen(port, ()=>{
     console.log('SERVER corriendo en http://localhost:5000');
 });
